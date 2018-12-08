ea330cd03c6a8750897f9fc23df8246abc8d517c
SystemsBotDream <45539387+SystemsBotDream@users.noreply.github.com>
2018-12-08 12:45:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -733,4 +733,14 @@ https://discord.gg/YRu4A7Z`)
      }
      });
 
+client.on('message', message => {
+var prefix = "$"
+        if(message.content.startsWith(prefix + 'hypixel')) {
+            let args = message.content.split(' ').slice(1).join(' ');
+            if (!args) return message.channel.send("**رجأء ضع اسمك في ماين كرافت. ?**");
+            var link = (`https://hypixel.net/player/${args}`);
+            message.channel.send(link);
+        }
+    });
+
 client.login(process.env.BOT_TOKEN);
